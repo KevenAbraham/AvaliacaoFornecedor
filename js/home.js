df9b0ad8ceb49730a5799c2244c337b6730d898b
KevenAbraham <abraham.keven20@gmail.com>
2024-02-06 16:10:11 -0300

    creating home

---
+++ js/home.js
@@ -0,0 +1,9 @@
+$('.forn-btn').click(function(){
+    $('nav ul .forn-show').toggleClass("show");
+    $('nav ul .first').toggleClass("rotate");
+});
+
+$('.aval-btn').click(function(){
+    $('nav ul .aval-show').toggleClass("show1");
+    $('nav ul .second').toggleClass("rotate");
+});
