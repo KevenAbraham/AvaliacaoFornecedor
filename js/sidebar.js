4e702911a555e7962549927857c7557f1f1fa671
KevenAbraham <abraham.keven20@gmail.com>
2024-02-23 10:37:35 -0300

    adjusting links, supplier in folder, changing the JS sidebar action

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,10 @@
 $(".menu-item").click(function () {
     var $currentSubMenu = $(this).find(".sub-menu");
     $(".sub-menu").not($currentSubMenu).slideUp();
+    $(".menu-item").not($(this)).removeClass("active");
+    $(".menu_vector").not($(this).find(".menu_vector")).css("transform", "none"); // Resetar a rotação do vetor dos outros itens do menu
     $currentSubMenu.slideToggle();
     $(this).toggleClass("active");
+ 
+    $(this).find(".menu_vector").css("transform", ""); // Resetar a rotação do vetor do item clicado
 });
